7c0992fdc8301adc88da4f5f87349e79d1ee4094
Freddy Chua <freddycct@gmail.com>
2016-10-08 12:53:29 -0700

    shifted files

--- encoder_decoder_iter.py
+++ encoder_decoder_iter.py
@@ -4,30 +4,37 @@ import numpy as np
 from collections import namedtuple
 from sklearn.cluster import KMeans
 
+EncDecBucketKey = namedtuple('EncDecBucketKey', ['enc_len', 'dec_len'])
+
 class EncoderDecoderBatch(object):
-    def __init__(self, all_data, all_label, init_states, bucket_key):
-        self.pad = 0 # at this point i do not know what is this for...
-
-        #all_data.shape is (x,y,z)
-        self.batch_size = all_data.shape[0]
-
+    def __init__(self, all_data, all_mask, all_label, init_states, bucket_key):
         # provide data, essential assignment
-        self.data = [ mx.nd.array(all_data) ]
-
-        # essential assignment
-        self.provide_data = [('data', (self.batch_size, bucket_key))]
-        for x in init_states:
-            self.data.append(mx.nd.zeros(x[1])) # x[1] is the shape of the initial data
-            self.provide_data.append(x)
+        # need to optimize this list creation!!!
+        self.data = [ mx.nd.array(all_data), mx.nd.array(all_mask) ] + [ mx.nd.zeros(x[1]) for x in init_states ]
 
         # provide label, essential assignment
         self.label = [ mx.nd.array(all_label) ]
-        self.provide_label = [ ('label', (self.batch_size, bucket_key)) ]
 
         self.init_states = init_states
 
         # bucket_key is essential for this databatch
         self.bucket_key = bucket_key
+
+        #all_data.shape is (x,y,z)
+        self.batch_size = all_data.shape[0]
+
+    # this two properties are essential too!
+    @property
+    def provide_data(self):
+        return [
+            ('data', (self.batch_size, self.bucket_key.enc_len + self.bucket_key.dec_len)),
+            ('mask', (self.batch_size, self.bucket_key.enc_len + self.bucket_key.dec_len))
+        ] + self.init_states
+
+    @property
+    def provide_label(self):
+        return [('label', (self.batch_size, self.bucket_key.dec_len))]
+
 
 def synchronize_batch_size(train_iter, test_iter):
     batch_size = min(train_iter.batch_size, test_iter.batch_size)
@@ -36,6 +43,7 @@ def synchronize_batch_size(train_iter, test_iter):
     train_iter.generate_init_states()
     test_iter.generate_init_states()
 
+
 # now define the bucketing, padding and batching SequenceIterator...
 class EncoderDecoderIter(mx.io.DataIter):
     def __init__(self, data_label, word2idx, idx2word, num_hidden, num_layers,
@@ -43,8 +51,12 @@ class EncoderDecoderIter(mx.io.DataIter):
 
         super(EncoderDecoderIter, self).__init__() # calling DataIter.__init__()
 
-        # data is a numpy array of 3 dimensions, #, timesteps, vector_dim
-        self.data_label = data_label
+        # data is a numpy array of 3 dimensions, (#, timesteps, vector_dim)
+        # let's say you have 2 sequences, #1 has len 5, dimension 10
+        self.data_label = data_label #numpy multi-dimensional array
+
+        # data_label[i] is the ith sequence
+
 
         self.word2idx = word2idx
         self.idx2word = idx2word
@@ -52,6 +64,8 @@ class EncoderDecoderIter(mx.io.DataIter):
         self.num_hidden = num_hidden
         self.num_layers = num_layers
         self.num_buckets = num_buckets
+
+        # arrange the data so that
 
         # now we need to find the buckets based on the input data...
         self.buckets, self.buckets_count, self.assignments = self.generate_buckets()
@@ -69,35 +83,45 @@ class EncoderDecoderIter(mx.io.DataIter):
         self.init_states = self.init_states_function(self.num_layers, self.num_hidden, self.batch_size)
 
     def generate_buckets(self):
-        enc_dec_data = [ len(data)+len(label)-1 for data, label in self.data_label ]
-        enc_dec_data = np.reshape(np.array(enc_dec_data), (-1, 1))
+        enc_dec_data = []
+        for data, label in self.data_label:
+            enc_len = len(data) - 1 # minue one because of the <EOS>
+            dec_len = len(label)
+            enc_dec_data.append((enc_len, dec_len))
 
-        kmeans = KMeans(n_clusters=self.num_buckets, random_state=1) # use clustering to decide the buckets
+        enc_dec_data = np.array(enc_dec_data)
+
+        kmeans = KMeans(n_clusters = self.num_buckets, random_state = 1) # use clustering to decide the buckets
         assignments = kmeans.fit_predict(enc_dec_data) # get the assignments
 
         # get the max of every cluster
-        buckets = np.array([np.amax(enc_dec_data[assignments==i]) for i in range(self.num_buckets)])
+        buckets = np.array([np.max( enc_dec_data[assignments==i], axis=0 ) for i in range(self.num_buckets)])
 
         # get # of sequences in each bucket... then assign the batch size as the minimum(minimum(bucketsize), batchsize)
-        buckets_count = np.array([enc_dec_data[assignments==i].shape[0] for i in range(self.num_buckets)])
+        buckets_count = np.array( [ enc_dec_data[assignments==i].shape[0] for i in range(self.num_buckets) ] )
 
         return buckets, buckets_count, assignments
 
     @property
     def default_bucket_key(self):
-        return np.amax(self.buckets)
+        enc_len, dec_len = np.max(self.buckets, axis=0)
+        return EncDecBucketKey(enc_len = enc_len, dec_len = dec_len)
 
     @property
     def provide_data(self): # this is necessary when specifying custom DataIter
         # length of data variable is length of encoder + length of decoder
-        bucket_key = self.default_bucket_key
-        return [('data', (self.batch_size, bucket_key))] + self.init_states
+        enc_dec_bucket_key = self.default_bucket_key
+
+        return [
+            ('data', (self.batch_size, enc_dec_bucket_key.enc_len + enc_dec_bucket_key.dec_len)),
+            ('mask', (self.batch_size, enc_dec_bucket_key.enc_len + enc_dec_bucket_key.dec_len))
+        ] + self.init_states
     #
     @property
     def provide_label(self): # this is necessary when specifying custom DataIter
         # length of label variable is only the length of decoder
-        bucket_key = self.default_bucket_key
-        return [('label', (self.batch_size, bucket_key))]
+        enc_dec_bucket_key = self.default_bucket_key
+        return [('label', (self.batch_size, enc_dec_bucket_key.dec_len))]
 
     # for custom DataIter, we must implement this class as an iterable and return a DataBatch
     def __iter__(self): # this is necessary to convert this class into an iterable
@@ -107,26 +131,29 @@ class EncoderDecoderIter(mx.io.DataIter):
         if self.iter_next():
             # suppose to get self.cursor:self.cursor + self.batch_size
             batch = self.data_label[self.assignments == self.cur_permute_bucket]\
-                [self.in_bucket_permutation[self.cursor:self.cursor+self.batch_size]]
+                [ self.in_bucket_permutation[self.cursor:self.cursor+self.batch_size] ]
 
             # get size of this bucket
-            seqlen = self.buckets[self.cur_permute_bucket] # this seqlen already deducted the <EOS>
+            enc_len, dec_len = self.buckets[self.cur_permute_bucket] # this enc_len already deducted the <EOS>
+            # total length of rnn sequence is enc_len+dec_len
 
-            all_data = np.full((self.batch_size, seqlen), self.pad_label, dtype=float)
-            all_label = np.full((self.batch_size, seqlen), self.pad_label, dtype=float)
+            all_data  = np.full((self.batch_size, enc_len+dec_len), self.pad_label, dtype=float)
+            all_label = np.full((self.batch_size, dec_len), self.pad_label, dtype=float)
+            all_mask = np.zeros((self.batch_size, enc_len+dec_len), dtype=float)
 
             for i, (data, label) in enumerate(batch):
                 if self.rev:
                     # reverse the input except for the <EOS> at end of input
                     # according to Ilya Sutskever et al. Sequence to Sequence Learning with Neural Networks
-                    # there is a reason for this... which you should ask freddy
                     data[:-1] = np.flipud(data[:-1])
 
-                all_data[i, :data.shape[0]] = data
-                all_data[i, data.shape[0]:data.shape[0]+label.shape[0]-1] = label[:-1]
-                all_label[i, data.shape[0]-1:data.shape[0]-1+label.shape[0]] = label
+                enc_input = np.concatenate((data, label[:-1])) # data <EOS> label
+                z = enc_len - data.shape[0] + 1
+                all_data[i, z:enc_len + label.shape[0]] = enc_input
+                all_mask[i, z:enc_len + label.shape[0]] = 1.0
+                all_label[i, :label.shape[0]] = label
 
-            return EncoderDecoderBatch(all_data, all_label, self.init_states, seqlen)
+            return EncoderDecoderBatch(all_data, all_mask, all_label, self.init_states, EncDecBucketKey(enc_len=enc_len, dec_len=dec_len))
         else:
             raise StopIteration
 
--- lstm.py
+++ lstm.py
@@ -6,11 +6,10 @@ from collections import namedtuple
 LSTMState = namedtuple("LSTMState", ["c", "h"])
 LSTMParam = namedtuple("LSTMParam", ["i2h_weight", "i2h_bias", "h2h_weight", "h2h_bias"])
 
-def lstm_cell(num_hidden, indata, prev_state, param, seqidx, layeridx, dropout=0.):
+def lstm_cell_no_mask(num_hidden, indata, prev_state, param, seqidx, layeridx, dropout=0.):
     """LSTM Cell symbol"""
     if dropout > 0.:
         indata = mx.sym.Dropout(data=indata, p=dropout)
-
     i2h = mx.sym.FullyConnected(
         data=indata,
         weight=param.i2h_weight,
@@ -40,6 +39,43 @@ def lstm_cell(num_hidden, indata, prev_state, param, seqidx, layeridx, dropout=0
 
     return LSTMState(c=next_c, h=next_h)
 
+def lstm_cell(num_hidden, indata, mask, prev_state, param, seqidx, layeridx, dropout=0.):
+    """LSTM Cell symbol"""
+    if dropout > 0.:
+        indata = mx.sym.Dropout(data=indata, p=dropout)
+    i2h = mx.sym.FullyConnected(
+        data=indata,
+        weight=param.i2h_weight,
+        bias=param.i2h_bias,
+        num_hidden=num_hidden * 4,
+        name="t%d_l%d_i2h" % (seqidx, layeridx)
+    )
+
+    h2h = mx.sym.FullyConnected(
+        data=prev_state.h,
+        weight=param.h2h_weight,
+        bias=param.h2h_bias,
+        num_hidden=num_hidden * 4,
+        name="t%d_l%d_h2h" % (seqidx, layeridx)
+    )
+
+    gates = i2h + h2h
+    slice_gates = mx.sym.SliceChannel(gates, num_outputs=4, name="t%d_l%d_slice" % (seqidx, layeridx))
+
+    in_gate = mx.sym.Activation(slice_gates[0], act_type="sigmoid")
+    in_transform = mx.sym.Activation(slice_gates[1], act_type="tanh")
+    forget_gate = mx.sym.Activation(slice_gates[2], act_type="sigmoid")
+    out_gate = mx.sym.Activation(slice_gates[3], act_type="sigmoid")
+
+    next_c = (forget_gate * prev_state.c) + (in_gate * in_transform)
+    next_h = out_gate * mx.sym.Activation(next_c, act_type="tanh")
+
+    # mask out the output
+    next_c = mx.sym.element_mask(next_c, mask)
+    next_h = mx.sym.element_mask(next_h, mask)
+
+    return LSTMState(c=next_c, h=next_h)
+
 def init_lstm(num_layer, prefix=''):
     param_cells = []
     last_states = []
@@ -60,47 +96,65 @@ def init_lstm(num_layer, prefix=''):
         )
     return param_cells, last_states
 
-def lstm_unroll(num_layer, seqlen, num_hidden, num_labels, dropout=0.0):
-    cls_weight   = mx.sym.Variable("cls_weight")
-    cls_bias     = mx.sym.Variable("cls_bias")
-    embed_weight = mx.sym.Variable("embed_weight")
+def lstm_unroll(num_lstm_layer, enc_len, dec_len, num_hidden, num_labels, dropout=0.0):
+    cls_weight = mx.sym.Variable("cls_weight")
+    cls_bias = mx.sym.Variable("cls_bias")
+    embed_weight=mx.sym.Variable("embed_weight")
 
-    param_cells, last_states = init_lstm(num_layer)
-    data = mx.sym.Variable('data')
+    enc_param_cells, last_states = init_lstm(num_lstm_layer, prefix='enc')
+    dec_param_cells, _ = init_lstm(num_lstm_layer, prefix='dec')
+
+    data  = mx.sym.Variable('data')
     label = mx.sym.Variable('label')
+    mask  = mx.sym.Variable('mask')
+
+    # (batch, time, vec) so axis 1 is the time step
 
     embed = mx.sym.Embedding(
-        data=data, # the idx to the embedding
-        input_dim=num_labels, # the number of rows for embed_weight
-        weight=embed_weight,  # the matrix representing the idx2vec
-        output_dim=num_hidden, # the number of cols for embed_weight
-        name='embed'
+        data=data, input_dim=num_labels,
+        weight=embed_weight, output_dim=num_hidden, name='embed'
     )
 
-    wordvec = mx.sym.SliceChannel(data=embed, num_outputs=seqlen, squeeze_axis=1)
+    # num_hidden = 10
+    # data is a sequence of index (0,1,2,3)
+    # embedding -> 10 x 4 matrix...
+    # each column is the word embedding vector
+
+    wordvec = mx.sym.SliceChannel(data=embed, num_outputs=enc_len + dec_len, squeeze_axis=1)
+    maskvec   = mx.sym.SliceChannel(data=mask,  num_outputs=enc_len + dec_len, squeeze_axis=1)
+
+    # numpy array is (2,3) => (2,3)
+    # numpy array is (1,3) => (3,)
 
     hidden_all = []
-    for seqidx in range(seqlen):
+    for seqidx in range(enc_len + dec_len):
         hidden = wordvec[seqidx]
+        mask_in = maskvec[seqidx]
+
         # stack LSTM
-        for i in range(num_layer):
+        for i in range(num_lstm_layer):
             dp = 0.0 if i == 0 else dropout
+
+            # encoder RNN
             next_state = lstm_cell(
                 num_hidden,
-                indata=hidden,
-                prev_state=last_states[i],
-                param=param_cells[i],
-                seqidx=seqidx,
-                layeridx=i,
-                dropout=dp
+                indata     = hidden,
+                mask       = mask_in,
+                prev_state = last_states[i],
+                param      = enc_param_cells[i] if seqidx < enc_len else dec_param_cells[i],
+                seqidx     = seqidx,
+                layeridx   = i,
+                dropout    = dp
             )
+
             hidden = next_state.h
             last_states[i] = next_state
-        # decoder
+
         if dropout > 0.0:
             hidden = mx.sym.Dropout(data=hidden, p=dropout)
 
-        hidden_all.append(hidden)
+        if(seqidx >= enc_len):
+            hidden_all.append(hidden)
 
     hidden_concat = mx.sym.Concat(*hidden_all, dim=0)
     pred = mx.sym.FullyConnected(
@@ -110,6 +164,16 @@ def lstm_unroll(num_layer, seqlen, num_hidden, num_labels, dropout=0.0):
         bias=cls_bias,
         name='pred'
     )
+
+    # hidden comes from lstm... output of 1 lstm is a vector...
+    # hidden_concat -> every column is the vector of each lstm at each time step
+
+    # softmax = e^(- w' * x) / \sum e^(- w' * x) <-- richard thinks this is softmax
+    # softmax = e^(-x) / \sum_i e^(-x_i)
+
+    # output of 1 lstm (top) is a vector of H dimensions...
+    # SoftmaxOutput => e^(x) / (1+e^(x)), x is a scalar
+    # output of lstm is a vector
 
     label = mx.sym.transpose(data=label) # e.g. if shape is (1,M) it becomes (M,1)
     label = mx.sym.Reshape(data=label, shape=(-1,)) # if shape is (M,1) it becomes (M,)
@@ -123,8 +187,8 @@ def lstm_unroll(num_layer, seqlen, num_hidden, num_labels, dropout=0.0):
     return output
 
 def get_lstm_sym_generator(num_layers, num_hidden, num_labels, dropout=0.0):
-    def generate_lstm_sym(seqlen):
-        return lstm_unroll(num_layers, seqlen, num_hidden, num_labels, dropout)
+    def generate_lstm_sym(bucketkey):
+        return lstm_unroll(num_layers, bucketkey.enc_len, bucketkey.dec_len, num_hidden, num_labels, dropout)
     return generate_lstm_sym
 
 def get_lstm_init_states(num_layers, num_dim, batch_size=1):
